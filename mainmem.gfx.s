a6a8b71067aedb89215724b055c98115f06d85e1
Bobbi Webber-Manners <bobbi.manners@gmail.com>
2021-10-10 14:43:57 -0400

    Minor refactor of mainmem.gfx.s

--- mainmem.gfx.s
+++ mainmem.gfx.s
@@ -19,9 +19,8 @@ SETLINE     >>>   ENTMAIN
             JSR   FDRAWADDR+43      ; FDRAW: SetLineMode
             >>>   XF2AUX,VDU18RET1
 
-* Call FDraw DrawLine routine
-DRAWLINE    >>>   ENTMAIN
-            LDA   PLOTMODE
+* Helper function to set up colors
+SETCOLOR    LDA   PLOTMODE
             AND   #$03
             CMP   #$01              ; Draw in foreground colour
             BNE   :S1
@@ -35,28 +34,30 @@ DRAWLINE    >>>   ENTMAIN
             BRA   :SETCOLOR
 :S2         LDA   BGCOLOR           ; Draw in background colour
 :SETCOLOR   STA   FDRAWADDR+5
-            JSR   FDRAWADDR+16      ; FDRAW: SetColor
+            JMP   FDRAWADDR+16      ; FDRAW: SetColor
+
+* Call FDraw DrawLine routine
+DRAWLINE    >>>   ENTMAIN
+            JSR   SETCOLOR
             JSR   FDRAWADDR+28      ; FDRAW: DrawLine
             >>>   XF2AUX,VDU25RET
 
 * Call FDraw DrawPoint routine
 DRAWPNT     >>>   ENTMAIN
-            LDA   PLOTMODE
-            AND   #$03
-            CMP   #$01              ; Draw in foreground colour
-            BNE   :S1
-            LDA   FGCOLOR
-            BRA   :SETCOLOR
-:S1         CMP   #$02              ; Draw in inverse colour
-            BNE   :S2
-            SEC
-            LDA   #$07
-            SBC   FGCOLOR
-            BRA   :SETCOLOR
-:S2         LDA   BGCOLOR           ; Draw in background colour
-:SETCOLOR   STA   FDRAWADDR+5
-            JSR   FDRAWADDR+16      ; FDRAW: SetColor
+            JSR   SETCOLOR
             JSR   FDRAWADDR+25      ; FDRAW: DrawPoint
+            >>>   XF2AUX,VDU25RET
+
+* Call FDraw DrawCircle routine
+DRAWCIRC    >>>   ENTMAIN
+            JSR   SETCOLOR
+            JSR   FDRAWADDR+37      ; FDRAW: DrawCircle
+            >>>   XF2AUX,VDU25RET
+
+* Call FDraw FillCircle routine
+FILLCIRC    >>>   ENTMAIN
+            JSR   SETCOLOR
+            JSR   FDRAWADDR+40      ; FDRAW: FillCircle
             >>>   XF2AUX,VDU25RET
 
 * Reset colours and linetype
