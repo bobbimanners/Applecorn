2ba4c6fc37abdd1d1ca3953f250566ff598229f8
Bobbi Webber-Manners <bobbi.manners@gmail.com>
2022-09-17 01:43:59 -0400

    Fixed clear to EOL for MODE 1 / HGR graphics.

--- mainmem.gfx.s
+++ mainmem.gfx.s
@@ -177,19 +177,20 @@ HGRSCR1L    >>>   ENTMAIN
             BNE   :L1
             >>>   XF2AUX,HSCR1RET
 
-* Clear one text line on HGR screen
-HCLRLINE    >>>   ENTMAIN
+* Clear one text line on HGR screen,
+* from current cursor col to text window right limit
+HCLREOL     >>>   ENTMAIN
+            INC   MTXTWINRGT
             LDA   HGRADDR+0         ; HGRADDR -> A4L,A4H
             STA   A4L
             LDA   HGRADDR+1
             STA   A4H
             LDA   #$00
             LDX   #$00
-:L1         LDY   #$00
+:L1         LDY   MVDUTEXTX
 :L2         STA   (A4L),Y
             INY
-*            CPY   #$39
-            CPY   #40
+            CPY   MTXTWINRGT
             BNE   :L2
             INC   A4H
             INC   A4H
@@ -205,35 +206,11 @@ BGCOLOR     DB    $00               ; Background colour
 LINETYPE    DB    $00               ; 0 normal, 1 XOR
 PLOTMODE    DB    $00               ; K value for PLOT K,X,Y
 HGRADDR     DW    $0000             ; Address 1st line of HGR char
+MVDUTEXTX   DB    $00               ; Main mem copy of VDUTEXTX
+MTXTWINRGT  DB    $00               ; Main mem copy of TEXTWINRGT
 
 * Addresses of start of pixel rows in PAGE1
 MHGRTAB     DW    $2000,$2080,$2100,$2180,$2200,$2280,$2300,$2380
             DW    $2028,$20A8,$2128,$21A8,$2228,$22A8,$2328,$23A8
             DW    $2050,$20D0,$2150,$21D0,$2250,$22D0,$2350,$23D0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
