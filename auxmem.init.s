90caff73740e06b0c1c30da80b8a2f1cad2af726
Bobbi Webber-Manners <bobbi.manners@gmail.com>
2022-09-22 19:56:17 -0400

    Added *DUMP (hexdump) command.

--- auxmem.init.s
+++ auxmem.init.s
@@ -205,7 +205,7 @@ BYTE00      BEQ   BYTE00A                    ; OSBYTE 0,0 - generate error
             RTS                              ; %000x1xxx host type, 'A'pple
 BYTE00A     BRK
             DB    $F7
-HELLO       ASC   'Applecorn MOS 2022-09-20'
+HELLO       ASC   'Applecorn MOS 2022-09-22'
             DB    $00                        ; Unify MOS messages
 
 
