c67c42aedb90d4ec393318d46d6073a5752f13e7
jgharston <jgh@mdfs.net>
2022-12-10 22:59:30 +0000

    Updated memory map and Equates

--- mainmem.fsequ.s
+++ mainmem.fsequ.s
@@ -1,40 +1,44 @@
 * MAINMEM.FSEQU.S
-* (c) Bobbi 2021 GPL v3
+* (c) Bobbi 2021-2022 GPL v3
 *
 * Constant definitions for ProDOS filesystem code that
 * resides in main memory.
 
 * ProDOS string buffers
 RTCBUF      EQU   $0200       ; Use by RTC calls, 40 bytes
-*                                 ; $0228-$023D
+*               ; $0228-$023D
 DRVBUF1     EQU   $023E
-DRVBUF2     EQU   $023F       ; Prefix on current drive, len+64
+DRVBUF2     EQU   $023F       ; Prefix on current drive, length
+*                 $0240       ; Prefix path
 CMDPATH     EQU   $0280       ; Path used to start Applecorn
+*               ; $02C0-$02FF
 
 * Filename string buffers
 MOSFILE1    EQU   $0300       ; length + 64 bytes
 MOSFILE2    EQU   $0341       ; length + 64 bytes
 MOSFILE     EQU   MOSFILE1
+*               ; $0382-$03A3 ROM selection w/s (move?)
+*               ; $03A4-$03BD
+
+* Control block for OSFILE
+FILEBLK     EQU   $03BE
+FBPTR       EQU   FILEBLK+0   ; Pointer to name (in aux)
+FBLOAD      EQU   FILEBLK+2   ; Load address (4 bytes)
+FBEXEC      EQU   FILEBLK+6   ; Exec address (4 bytes)
+FBSIZE      EQU   FILEBLK+10  ; Size (4 bytes)
+FBSTRT      EQU   FILEBLK+10  ; Start address for SAVE (4 bytes)
+FBATTR      EQU   FILEBLK+14  ; Attributes (4 bytes)
+FBEND       EQU   FILEBLK+14  ; End address for SAVE (4 bytes)
 
 * Control block for OSGBPB
-GBPBBLK     EQU   $0382
+GBPBBLK     EQU   FILEBLK+1
 GBPBHDL     EQU   GBPBBLK+0   ; File handle
 GBPBDAT     EQU   GBPBBLK+1   ; Pointer to data (4 bytes)
 GBPBNUM     EQU   GBPBBLK+5   ; Num bytes to transfer (4 bytes)
 GBPBPTR     EQU   GBPBBLK+9   ; File pointer (4 bytes)
 GBPBAUXCB   EQU   GBPBBLK+13  ; Address of aux control block (2 bytes)
 
-*                 $0391       ; $2D bytes here
-
-* Control block for OSFILE
-FILEBLK     EQU   $03BE
-FBPTR       EQU   FILEBLK+0   ; Pointer to name (in aux)
-FBLOAD      EQU   FILEBLK+2   ; Load address
-FBEXEC      EQU   FILEBLK+6   ; Exec address
-FBSIZE      EQU   FILEBLK+10  ; Size
-FBSTRT      EQU   FILEBLK+10  ; Start address for SAVE
-FBATTR      EQU   FILEBLK+14  ; Attributes
-FBEND       EQU   FILEBLK+14  ; End address for SAVE
+*               ; $03D0-$03FF ; ProDOS workspace
 
 * ProDOS MLI command numbers
 ALLOCCMD    EQU   $40
@@ -57,56 +61,4 @@ FLSHCMD     EQU   $CD
 SMARKCMD    EQU   $CE
 GMARKCMD    EQU   $CF
 GEOFCMD     EQU   $D1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
